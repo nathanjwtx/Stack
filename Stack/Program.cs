00597b66579885616f5337d441df02f9c11d32b0
nathanjwtx <dynamonath@nathanjwtx.me>
2017-10-21 07:58:09 -0500

    Initial commit

---
+++ Stack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            var finished = false;
+            while (finished == false)
+            {
+                Console.WriteLine("(A)dd to stack, (R)emove item, or (E)mpty the stack?");
+                var response = Console.ReadLine().ToUpper();
+                switch (response)
+                {
+                    case "A":
+                        Console.WriteLine("Enter object to push: ");
+                        stack.Push(Console.ReadLine());
+                        break;
+                    case "R":
+                        Console.WriteLine(stack.Pop());
+                        break;
+                    case "E":
+                        stack.Clear();
+                        break;
+                    default:
+                        finished = true;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private List<object> NewStack { get; set; }
+
+        public Stack()
+        {
+            this.NewStack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                NewStack.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException("Passed null");
+            }
+        }
+
+        public object Pop()
+        {
+            var l = NewStack.Count;
+            if (l > 0)
+            {
+                var index = l - 1;
+                var toReturn = NewStack[index];
+                NewStack.RemoveAt(index);
+                return toReturn;
+            }
+            else
+            {
+                return new InvalidOperationException("Empty stack");
+            }
+        }
+
+        public void Clear()
+        {
+            NewStack.Clear();
+            Console.WriteLine("Stack is now empty");
+        }
+    }
+}
